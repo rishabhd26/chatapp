5ef72ba86ddb07cbce8ebf1ecc1f731ff34cd0cb
rishabhd26 <rishabhd26@gmail.com>
2016-12-18 13:29:23 +0530

    Add files via upload

---
+++ src/androidTest/java/com/example/rishabh/message/gcm/NotificationUtilsTest.java
@@ -0,0 +1,8 @@
+package com.example.rishabh.message.gcm;
+
+/**
+ * Created by rishabh on 28/11/16.
+ */
+public class NotificationUtilsTest {
+
+}
---
+++ src/test/java/com/example/rishabh/message/app/MyApplicationTest.java
@@ -0,0 +1,8 @@
+package com.example.rishabh.message.app;
+
+/**
+ * Created by rishabh on 20/10/16.
+ */
+public class MyApplicationTest {
+
+}
